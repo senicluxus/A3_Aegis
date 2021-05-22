adae626ae5e88a64c0ccd8f315ea1bd545c3cedb
Senicluxus <senicluxus@gmail.com>
2021-05-22 13:33:33 -0400

    Initial Commit

---
+++ A3_Aegis/characters_f_aegis/Uniforms/cfgIndep.hpp
@@ -0,0 +1,15 @@
+/* AAF */
+class U_I_CombatUniform_tshirt: Uniform_Base
+{
+	scope = public;
+    displayName = $STR_A3_A_CfgWeapons_U_I_CombatUniform_tshirt0;
+	picture = "\A3_Aegis\Characters_F_Aegis\Uniforms\Data\UI\icon_U_I_CombatUniform_tshirt_CA.paa";
+	class ItemInfo: UniformItem
+	{
+		uniformClass = I_support_MG_F;
+	};
+};
+class U_I_pilotCoveralls: Uniform_Base
+{  
+    picture = "\A3\Characters_F\Data\UI\icon_U_IR_PilotCoveralls_CA.paa";
+};
