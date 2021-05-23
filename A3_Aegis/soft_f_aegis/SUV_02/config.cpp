3892b2a16666107d74da84d2ed4eb9932eaa2999
Senicluxus <senicluxus@gmail.com>
2021-05-23 17:27:54 -0400

    Comment out ION WIP SUV

--- A3_Aegis/soft_f_aegis/SUV_02/config.cpp
+++ A3_Aegis/soft_f_aegis/SUV_02/config.cpp
@@ -4,9 +4,9 @@
 /* Definitions & Inheritance */
 #include "\A3_Aegis\basicDefines_A3.hpp"
 #include "\A3_Aegis\sensors.hpp"
-#include "DeltagamerDefines.hpp"				// Dega SUV Defines
-#include "config_crewanimation.hpp"		        //
+//#include "DeltagamerDefines.hpp"				// Dega SUV Defines
+//#include "config_crewanimation.hpp"		        //
 
 /* Configuration */
-#include "cfgVehicles.hpp"
-#include "cfgWeapons.hpp"
+//#include "cfgVehicles.hpp"
+//#include "cfgWeapons.hpp"
