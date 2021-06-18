105fdb9e4e468fc51cddefcaa3f9d0f88742283d
BranFlakes2035 <78566121+BranFlakes2035@users.noreply.github.com>
2021-06-17 22:01:49 -0700

    UN Covered Raven Helmet
    Added Covered Raven helmet.

--- A3_Aegis/characters_f_aegis/Headgear/cfgWeapons.hpp
+++ A3_Aegis/characters_f_aegis/Headgear/cfgWeapons.hpp
@@ -2216,6 +2216,18 @@ class CfgWeapons
 			};
 	  	};
 	};
+	class H_HelmetSpecter_cover_UNO_F: H_HelmetSpecter_cover_base_F
+	{
+		author = $STR_A3_A_AveryTheKitty_Deathstruck_and_Midnas;
+		scope = public;
+		displayName = $STR_A3_A_CfgWeapons_H_HelmetSpecter_cover_UNO_F0;
+		//picture = "\A3_Opf\Characters_F_Opf\Headgear\Data\UI\icon_H_HelmetSpecter_cover_khaki_F_CA.paa";
+		hiddenSelectionsTextures[] =
+		{
+			"\A3_Aegis\Characters_F_Aegis\Headgear\Data\H_HelmetSpecter_01_CO.paa",
+			"\A3_Aegis\Characters_F_Aegis\Headgear\Data\H_HelmetSpecter_01_UNO_cover.paa"
+		};
+	};
 	class H_HelmetSpecter_paint_F: H_HelmetSpecter_base_F
 	{
 		author = $STR_A3_A_AveryTheKitty_Deathstruck_and_Midnas;
