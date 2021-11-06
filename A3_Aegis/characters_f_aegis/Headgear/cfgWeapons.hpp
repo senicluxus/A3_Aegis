7a8d8601111a34d1b0354f0195cb843ce5e84b4f
Ravenholme <61998820+Ravenholme@users.noreply.github.com>
2021-11-06 18:45:21 +0000

    Fixes

--- A3_Aegis/characters_f_aegis/Headgear/cfgWeapons.hpp
+++ A3_Aegis/characters_f_aegis/Headgear/cfgWeapons.hpp
@@ -2638,28 +2638,21 @@ class CfgWeapons
          	"\A3_Aegis\Characters_F_Aegis\Headgear\Data\equip_d_co.paa"
         };
     };
-	class H_MK7_blue_F: H_MK7_Base_F
+	class H_MK7_UN_F: H_MK7_Base_F
     {
         author = "Bran Flakes and TacticalDruid";
         scope = public;
-        displayName = $STR_A3_A_CfgWeapons_H_MK7_blue_F0;
+        displayName = $STR_A3_A_CfgWeapons_H_MK7_UN_F0;
         //picture = "Picture path";
         model = "\A3_Aegis\Characters_F_Aegis\Headgear\H_Mk7_F";
         hiddenSelections[] = { "Camo","Camo2","Camo3" };
         hiddenSelectionsTextures[] =
         {
-         	"\A3_Aegis\Characters_F_Aegis\Headgear\Data\MK7_blue.tga",
+         	"\A3_Aegis\Characters_F_Aegis\Headgear\Data\MK7_UN.tga",
          	"\A3_Aegis\Characters_F_Aegis\Headgear\Data\H_HelmetSpecter_01_brown_CO.paa",
          	"\A3_Aegis\Characters_F_Aegis\Headgear\Data\equip_d_co.paa"
         };
     };
-	class H_I_Helmet_canvas_Blue_F: H_HelmetIA
-	{
-	  	author = $STR_A3_A_BranFlakes;
-		displayName = $STR_A3_A_CfgWeapons_H_I_Helmet_canvas_Blue_F0;
-		picture = "\A3_Aegis\Characters_F_Aegis\Headgear\Data\UI\icon_H_I_Helmet_canvas_Blue_CA.paa";
-		hiddenSelectionsTextures[] = {"\A3_Aegis\Characters_F_Aegis\Headgear\Data\headgear_helmet_canvas_Blue_CO.paa"};
-	};
 	class H_I_Helmet_canvas_UN_F: H_HelmetIA
 	{
 	  	author = $STR_A3_A_BranFlakes;
