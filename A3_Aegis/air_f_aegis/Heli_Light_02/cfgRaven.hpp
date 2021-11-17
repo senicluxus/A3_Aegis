cc365a2274adff4d37b26d84fa7f948e9904015e
Ravenholme <61998820+Ravenholme@users.noreply.github.com>
2021-11-17 23:09:25 +0000

    Raven Security (OPFOR) added

--- A3_Aegis/air_f_aegis/Heli_Light_02/cfgRaven.hpp
+++ A3_Aegis/air_f_aegis/Heli_Light_02/cfgRaven.hpp
@@ -1,3 +1,4 @@
+/* Raven - INDEP */
 class I_Raven_Heli_Light_02_dynamicLoadout_ard_F: O_R_Heli_Light_02_dynamicLoadout_ard_F
 {
 	author = $STR_A3_A_Heliotrope;
@@ -25,4 +26,20 @@ class I_Raven_Heli_Light_02_unarmed_ard_F: O_R_Heli_Light_02_unarmed_ard_F
 		Green,1
 	};
 	hiddenSelectionsTextures[] = {"\A3_Aegis\Air_F_Aegis\Heli_Light_02\Data\Heli_Light_02_ext_raven_CO.paa"};
+};
+
+/* Raven - OPFOR */
+class O_Raven_Heli_Light_02_dynamicLoadout_F: I_Raven_Heli_Light_02_dynamicLoadout_ard_F
+{
+	faction = OPF_Raven_F;
+	side = TEast;
+	crew = O_Raven_Soldier_helipilot_F;
+	typicalCargo[] = {O_Raven_Soldier_helipilot_F};
+};
+class O_Raven_Heli_Light_02_unarmed_F: I_Raven_Heli_Light_02_unarmed_ard_F
+{
+	faction = OPF_Raven_F;
+	side = TEast;
+	crew = O_Raven_Soldier_helipilot_F;
+	typicalCargo[] = {O_Raven_Soldier_helipilot_F};
 };
