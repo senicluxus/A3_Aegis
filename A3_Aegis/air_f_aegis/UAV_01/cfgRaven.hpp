293e7fafb6be0574b7c4f792b982ced266c07378
Ravenholme <61998820+Ravenholme@users.noreply.github.com>
2021-12-05 13:11:33 +0000

    Raven Security Darter Bag fix

--- A3_Aegis/air_f_aegis/UAV_01/cfgRaven.hpp
+++ A3_Aegis/air_f_aegis/UAV_01/cfgRaven.hpp
@@ -146,30 +146,8 @@ class O_Raven_UAV_01_F: UAV_01_base_F
 		base = "";
 		assembleTo = "";
 		displayName = "";
-		dissasembleTo[] = {O_Raven_UAV_01_backpack_F};
+		dissasembleTo[] = {O_R_UAV_01_backpack_F};
 	};
 	hiddenSelectionsTextures[] = {"\A3_Aegis\Air_F_Aegis\UAV_01\Data\UAV_01_RUgrey_CO.paa"};
 	textureList[] = {Rus,1};
-};
-class O_Raven_UAV_01_backpack_F: Weapon_Bag_Base
-{
-	author = $STR_A3_A_Heliotrope;
-	mapSize = 0.6;
-	scope = public;
-	scopeCurator = public;
-	displayName = $STR_A3_A_CfgVehicles_O_R_UAV_01_backpack_F0;
-	model = "\A3\Drones_F\Weapons_F_Gamma\Ammoboxes\Bags\UAV_backpack_F.p3d";
-	editorCategory = EdCat_Equipment;
-	editorSubcategory = EdSubcat_Backpacks;
-	faction = OPF_Raven_F;
-	picture = "\A3_Aegis\Supplies_F_Aegis\Bags\Data\UI\icon_O_R_UAV_01_backpack_F_CA.paa";
-	hiddenSelectionsTextures[] = {"\A3_Aegis\Supplies_F_Aegis\Bags\Data\UAV_backpack_RUkhk_CO.paa"};
-	maximumLoad = 0;
-	mass = 300;
-	class assembleInfo: assembleInfo
-	{
-		base = "";
-		displayName = $STR_A3_A_CfgVehicles_O_R_UAV_01_backpack_F_assembleInfo0;
-		assembleTo = O_Raven_UAV_01_F;
-	};
 };
