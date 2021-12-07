56a75318b3975271afeb4750ab78238bf5d167d4
Senicluxus <senicluxus@gmail.com>
2021-12-06 19:08:50 -0500

    USMC (Desert) progress

---
+++ A3_Aegis/characters_f_aegis/cfgUSMC.hpp
@@ -0,0 +1,277 @@
+class B_D_Soldier_universal_lxWS : B_Soldier_universal_F
+{
+   scope = protected;
+   scopeCurator = protected; 
+   hiddenSelectionsTextures[] = {"\lxws\characters_f_lxws\data\NATO\clothing1_desert_CO.paa"};
+   linkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert";
+};
+
+class B_D_soldier_AAR_lxWS : B_soldier_AAR_F
+{
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_soldier_AAR_lxWS.jpg";
+   linkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_light_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_light_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert_tshirt";
+   backpack = "B_Kitbag_desert_AAR_lxWS";
+};
+class B_D_support_AMG_lxWS : B_support_AMG_F
+{
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_support_AMG_lxWS.jpg";
+   linkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_light_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_light_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert_tshirt";
+   backpack = "B_D_HMG_01_support_lxWS";
+};
+class B_D_support_AMort_lxWS : B_support_AMort_F
+{
+   DEFAULT_D_SHIRT;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_support_AMort_lxWS.jpg";
+   linkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_light_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_light_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert_tshirt";
+   backpack = "B_D_Mortar_01_support_lxWS";
+};
+class B_D_soldier_AAA_lxWS : B_soldier_AAA_F
+{
+   DEFAULT_D_SHIRT;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_soldier_AAA_lxWS.jpg";
+   linkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_light_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_light_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert_tshirt";
+   backpack = "B_D_Carryall_mcamo_AAA_lxWS";
+};
+class B_D_soldier_AAT_lxWS : B_soldier_AAT_F
+{
+   DEFAULT_D_SHIRT;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_soldier_AAT_lxWS.jpg";
+   linkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_light_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_light_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert_tshirt";
+   backpack = "B_D_Carryall_mcamo_AAT_lxWS";
+};
+
+class B_D_soldier_AR_lxWS : B_soldier_AR_F
+{
+   DEFAULT_D_SHIRT;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_soldier_AR_lxWS.jpg";
+   linkedItems[] = {"V_PlateCarrier2_khk","H_HelmetB_desert","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_PlateCarrier2_khk","H_HelmetB_desert","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert_tshirt";
+};
+class B_D_medic_lxWS : B_medic_F
+{
+   DEFAULT_D_SHIRT;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_medic_lxWS.jpg";
+   linkedItems[] = {"V_lxWS_PlateCarrierSpec_desert","H_HelmetB_light_desert","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_lxWS_PlateCarrierSpec_desert","H_HelmetB_light_desert","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert_tshirt";
+   backpack = "B_AssaultPack_desert_Medic_lxWS";
+};
+
+class B_D_crew_lxWS : B_crew_F
+{
+   DEFAULT_D;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_crew_lxWS.jpg";
+   linkedItems[] = {"V_BandollierB_khk","H_HelmetCrew_B","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_BandollierB_khk","H_HelmetCrew_B","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert_vest";
+};
+class B_D_engineer_lxWS : B_engineer_F
+{
+   DEFAULT_D;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_engineer_lxWS.jpg";
+   linkedItems[] = {"V_Chestrig_khk","H_HelmetB_desert","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_Chestrig_khk","H_HelmetB_desert","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert_vest";
+   backpack = "B_Kitbag_desert_Eng_lxWS";
+};
+
+class B_D_soldier_exp_lxWS : B_soldier_exp_F
+{
+   DEFAULT_D;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_soldier_exp_lxWS.jpg";
+   linkedItems[] = {"V_lxWS_PlateCarrierGL_desert","H_HelmetSpecB_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_lxWS_PlateCarrierGL_desert","H_HelmetSpecB_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert";
+   backpack = "B_Kitbag_desert_Exp_lxWS";
+};
+
+class B_D_Soldier_GL_lxWS : B_Soldier_GL_F
+{
+   DEFAULT_D;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_Soldier_GL_lxWS.jpg";
+   linkedItems[] = {"V_lxWS_PlateCarrierGL_desert","H_HelmetSpecB_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_lxWS_PlateCarrierGL_desert","H_HelmetSpecB_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert";
+};
+class B_D_support_GMG_lxWS : B_support_GMG_F
+{
+   DEFAULT_D_SHIRT;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_support_GMG_lxWS.jpg";
+   linkedItems[] = {"V_Chestrig_khk","H_HelmetB_light_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_Chestrig_khk","H_HelmetB_light_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert_tshirt";
+   backpack = "B_D_GMG_01_weapon_lxWS";
+};
+class B_D_support_MG_lxWS : B_support_MG_F
+{
+   DEFAULT_D_SHIRT;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_support_MG_lxWS.jpg";
+   linkedItems[] = {"V_Chestrig_khk","H_HelmetB_light_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_Chestrig_khk","H_HelmetB_light_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert_tshirt";
+   backpack = "B_D_HMG_01_weapon_lxWS";
+};
+class B_D_support_Mort_lxWS : B_support_Mort_F
+{
+   DEFAULT_D_SHIRT;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_support_Mort_lxWS.jpg";
+   linkedItems[] = {"V_Chestrig_khk","H_HelmetB_light_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_Chestrig_khk","H_HelmetB_light_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert_tshirt";
+   backpack = "B_D_Mortar_01_weapon_lxWS";
+};
+
+class B_D_soldier_M_lxWS : B_soldier_M_F
+{
+   DEFAULT_D;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_soldier_M_lxWS.jpg";
+   linkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_desert","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_desert","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert";
+};
+class B_D_soldier_mine_lxWS : B_soldier_mine_F 
+{
+   DEFAULT_D;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_soldier_mine_lxWS.jpg";
+   linkedItems[] = {"V_lxWS_PlateCarrierGL_desert","H_HelmetSpecB_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_lxWS_PlateCarrierGL_desert","H_HelmetSpecB_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert";
+   backpack = "B_Carryall_desert_Mine_lxWS";
+};
+class B_D_soldier_AA_lxWS : B_soldier_AA_F
+{
+   DEFAULT_D;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_soldier_AA_lxWS.jpg";
+   linkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_light_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_light_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert";
+   backpack = "B_AssaultPack_desert_AA_lxWS";
+};
+class B_D_soldier_AT_lxWS : B_soldier_AT_F 
+{
+   DEFAULT_D;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_soldier_AT_lxWS.jpg";
+   linkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_light_desert","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_light_desert","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert";
+   backpack = "B_AssaultPack_desert_AT_lxWS";
+};
+class B_D_officer_lxWS : B_officer_F
+{
+   DEFAULT_D;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_officer_lxWS.jpg";
+   linkedItems[] = {"V_BandollierB_khk","lxWS_H_MilCap_desert","ItemGPS","ItemMap","ItemCompass","ItemWatch","ItemRadio"};
+   respawnLinkedItems[] = {"V_BandollierB_khk","lxWS_H_MilCap_desert","ItemGPS","ItemMap","ItemCompass","ItemWatch","ItemRadio"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert";
+};
+class B_D_soldier_PG_lxWS : B_soldier_PG_F
+{
+   DEFAULT_D;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_soldier_PG_lxWS.jpg";
+   linkedItems[] = {"V_lxWS_PlateCarrierSpec_desert","H_HelmetB_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_lxWS_PlateCarrierSpec_desert","H_HelmetB_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert";
+};
+class B_D_soldier_repair_lxWS : B_soldier_repair_F
+{
+   DEFAULT_D;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_soldier_repair_lxWS.jpg";
+   linkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_light_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_light_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert";
+   backpack = "B_AssaultPack_desert_Repair_lxWS";
+};
+
+class B_D_Soldier_lxWS : B_Soldier_F
+{
+   DEFAULT_D;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_Soldier_lxWS.jpg";
+   linkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert";
+};
+class B_D_soldier_LAT_lxWS : B_soldier_LAT_F
+{
+   DEFAULT_D;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_soldier_LAT_lxWS.jpg";
+   linkedItems[] = {"V_PlateCarrier2_khk","H_HelmetB_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_PlateCarrier2_khk","H_HelmetB_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"}; 
+   uniformClass = "U_lxWS_B_CombatUniform_desert";
+   backpack = "B_AssaultPack_desert_LAT_lxWS";
+};
+class B_D_soldier_LAT2_lxWS : B_soldier_LAT2_F
+{
+   DEFAULT_D;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_soldier_LAT2_lxWS.jpg";
+   linkedItems[] = {"V_PlateCarrier2_khk","H_HelmetB_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_PlateCarrier2_khk","H_HelmetB_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert";
+   backpack = "B_AssaultPack_desert_LAT2_lxWS";
+};
+
+class B_D_Soldier_lite_lxWS : B_Soldier_lite_F
+{
+   DEFAULT_D;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_Soldier_lite_lxWS.jpg";
+   linkedItems[] = {"V_BandollierB_khk","lxWS_H_MilCap_desert","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_BandollierB_khk","lxWS_H_MilCap_desert","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert_vest";
+};
+
+class B_D_Soldier_unarmed_lxWS : B_Soldier_unarmed_F
+{
+   DEFAULT_D;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_Soldier_unarmed_lxWS.jpg";
+   linkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_PlateCarrier1_khk","H_HelmetB_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert";
+};
+
+class B_D_Soldier_SL_lxWS : B_Soldier_SL_F
+{
+   DEFAULT_D;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_Soldier_SL_lxWS.jpg";
+   linkedItems[] = {"V_lxWS_PlateCarrierGL_desert","H_HelmetB_desert","ItemGPS","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_lxWS_PlateCarrierGL_desert","H_HelmetB_desert","ItemGPS","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert_vest";
+};
+
+class B_D_Soldier_TL_lxWS : B_Soldier_TL_F
+{
+   DEFAULT_D;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_Soldier_TL_lxWS.jpg";
+   linkedItems[] = {"V_lxWS_PlateCarrierGL_desert","H_HelmetSpecB_sand","ItemGPS","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   respawnLinkedItems[] = {"V_lxWS_PlateCarrierGL_desert","H_HelmetSpecB_sand","ItemGPS","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert_vest";
+};
+
+class B_D_soldier_UAV01_lxWS : B_soldier_UAV_F
+{
+   DEFAULT_D;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_soldier_UAV01_lxWS.jpg";
+   linkedItems[] = {"V_lxWS_PlateCarrierSpec_desert","H_HelmetB_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","B_UavTerminal","NVGoggles"};
+   respawnLinkedItems[] = {"V_lxWS_PlateCarrierSpec_desert","H_HelmetB_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","B_UavTerminal","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert";
+};
+
+class B_D_soldier_UAV02_lxWS : B_soldier_UAV_lxWS
+{
+   DEFAULT_D;
+   editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\B_D_soldier_UAV02_lxWS.jpg";
+   linkedItems[] = {"V_lxWS_PlateCarrierSpec_desert","H_HelmetB_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","B_UavTerminal","NVGoggles"};
+   respawnLinkedItems[] = {"V_lxWS_PlateCarrierSpec_desert","H_HelmetB_sand","ItemMap","ItemCompass","ItemWatch","ItemRadio","B_UavTerminal","NVGoggles"};
+   uniformClass = "U_lxWS_B_CombatUniform_desert";
+};
